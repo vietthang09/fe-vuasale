3dc936d430a574d1c6d991c798843a93009044ac
vietthang09 <vthcvn@gmail.com>
2024-07-28 20:32:55 +0700

    feat: change BASE_URl

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const baseURL = "http://127.0.0.1:5000";
-// export const baseURL = "https://api.vuasale.com";
+// export const baseURL = "http://127.0.0.1:5000";
+export const baseURL = "https://api.vuasale.com";
 // export const baseURL = "http://14.225.198.130:5000";
